bad90ae7360cb72df53a4030c8ae33342a4dffec
Vladimir Starkov <iamstarkov@gmail.com>
2015-06-25 21:08:42 +0200

    finishing last tests

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ const trimP  = input => compose(trim, join(''), filterIndexed(range(2, 3)), spli
 const trimH1 = input => compose(trim, join(''), filterIndexed(range(3, 4)), split(''), trim)(input || '');
 
 // matchers
-const isTitle = node => {
-  console.log(node.type, node.level, node.literal)
-  return isHeader(node) && isLevel(node, 1)
-};
+const isTitle = node => isHeader(node) && isLevel(node, 1);
 const isEmpty = node => !node.literal;
 const isValidDate = input => moment(new Date(input)).isValid();
 const isDate = node => node.literal && isValidDate(node.literal);
-const isDesc = (node, date) => !text(node).match(date) && isParagraph(node);
+const isDesc = (node, event, date) => !text(node).match(date) && isParagraph(node);
 const remove = (...matchers) => input => apply(compose, matchers.map(item => partialRight(matchRemove, item)))(input);
 
 const extract = (input) => {
@@ -26,11 +23,9 @@ const extract = (input) => {
   const date = match(input, isDate).literal;
   const sortableDate = new Date(date).getTime();
 
-  /*
   const descText = compose(text, partialRight(match, partialRight(isDesc, date)))(input);
   const descHtml = compose(trimP, html, partialRight(match, partialRight(isDesc, date)))(input);
 
-  */
   const image = (match(input, isImage) || {}).destination || '';
 
   const contentText = compose(text, remove(isEmpty, isTitle, isDate))(input);
@@ -39,9 +34,7 @@ const extract = (input) => {
   return {
     titleText, titleHtml,
     date, sortableDate,
-    /*
     descText, descHtml,
-    */
     image,
     contentText, contentHtml
   };
