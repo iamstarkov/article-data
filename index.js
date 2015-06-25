ce1e36f36d7c5acda53cbb8d7d4c51ee52b8f9ce
Vladimir Starkov <iamstarkov@gmail.com>
2015-06-26 00:07:51 +0200

    small refactoring

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import moment from 'moment';
 // helpers
 const range = (start, end) =>
   (item, i, arr) => i > start && i < arr.length - 1 - end;
-
 const trimP  = input => compose(trim, join(''), filterIndexed(range(2, 3)), split(''), trim)(input || '');
 const trimH1 = input => compose(trim, join(''), filterIndexed(range(3, 4)), split(''), trim)(input || '');
 
@@ -16,28 +15,25 @@ const isDesc = (node, date) => !text(node).match(date) && isParagraph(node);
 const isEmpty = node => !node.literal;
 
 const extract = (input) => {
-  const title = match(input, isTitle);
   const date = (match(input, isDate) || {}).literal;
-  const desc = match(input, node => isDesc(node, date));
-  const image = (match(input, isImage) || {}).destination;
-  const content = matchRemoveList(input, isEmpty, isTitle, isDate);
-  return { title, date, desc, image, content };
+  return {
+    title: match(input, isTitle), date,
+    desc: match(input, node => isDesc(node, date)),
+    image: (match(input, isImage) || {}).destination,
+    content: matchRemoveList(input, isEmpty, isTitle, isDate)
+  };
 };
 
 export default (input) => {
   const article = extract(input);
   return {
-    titleText:   trim(text(article.title)),
+    titleText: trim(text(article.title)),
     titleHtml: trimH1(html(article.title)),
-
     date: article.date,
     sortableDate: new Date(article.date || '').getTime(),
-
     descText: text(article.desc),
     descHtml: trimP(html(article.desc)),
-
     image: (article.image || ''),
-
     contentText: text(article.content),
     contentHtml: html(article.content),
   };
