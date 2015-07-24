c11b59e19fea95f02f69cc2c1f7252719c074375
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-24 08:02:16 +0500

    remove legacy check

--- index.js
+++ index.js
@@ -13,17 +13,13 @@ const trimH1 = input => compose(trim, join(''), filterIndexed(range(3, 4)), spli
 
 // matchers
 const isTitle = node => isHeader(node) && isLevel(node, 1);
-const isDate = node => node.literal && moment(new Date(node.literal)).isValid();
 const isDesc = (node, date) => !text(node).match(date) && isParagraph(node);
 const isEmpty = node => !node.literal;
 
 const extract = (input) => {
-  const date = (match(input, isDate) || {}).literal;
   return {
-    date,
-    desc: match(input, node => isDesc(node, date)),
     image: (match(input, isImage) || {}).destination,
-    content: matchRemoveList(input, isEmpty, isTitle, isDate)
+    content: matchRemoveList(input, isEmpty, isTitle)
   };
 };
 
@@ -32,10 +28,7 @@ export default (input) => {
   return {
     title: getTitle(input),
     date: getDate('DD MMM YYYY', 'en', input),
-    sortableDate: new Date(article.date || '').getTime(),
     desc: getDesc(getDate('DD MMM YYYY', 'en', input).text, input),
-    descText: text(article.desc),
-    descHtml: trimP(html(article.desc)),
     image: (article.image || ''),
     contentText: text(article.content),
     contentHtml: html(article.content),
