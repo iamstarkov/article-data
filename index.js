e5186d9aca78d047459a0b7e82a67bc0cd739cf3
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-10 19:01:52 +0200

    complete refactoring

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-import { html, text, match, matchRemoveList, isHeader, isLevel } from 'commonmark-helpers';
-import getTitle from 'get-md-title';
-import getDate  from 'get-md-date';
-import getDesc  from 'get-md-desc';
-import getImage from 'get-md-image';
-import moment from 'moment';
+import getTitle   from 'get-md-title';
+import getDate    from 'get-md-date';
+import getDesc    from 'get-md-desc';
+import getImage   from 'get-md-image';
+import getContent from 'get-md-content';
 
-// matchers
-const isTitle = node => isHeader(node) && isLevel(node, 1);
-const isEmpty = node => !node.literal;
-
-const extract = (input) => {
-  return {
-    content: matchRemoveList(input, isEmpty, isTitle)
-  };
-};
-
-// что такое контент?
-// это статья без нод заголовка, дескрпшена и ноды содержащей дату
-
-export default (input) => {
-  const article = extract(input);
-  const pattern = 'DD MMMM YYYY';
-  const locale = 'en';
-  return {
-    title: getTitle(input),
-    date: getDate(pattern, locale, input),
-    desc: getDesc(getDate(pattern, locale, input).text, input),
-    image: getImage(input).src ? getImage(input) : { src: '', alt: '' },
-    // contentText: text(article.content),
-    // contentHtml: html(article.content),
-  };
+export default (input, dateFormat, dateLocale) => {
+  const title   = getTitle(input);
+  const date    = getDate(dateFormat, dateLocale, input);
+  const desc    = getDesc(input, date.text);
+  const image   = getImage(input);
+  const content = getContent(input, [title.text, date.text]);
+  return { title, date, desc, content, image };
 };
--- test.js
+++ test.js
@@ -1,21 +1,39 @@
 import { equal } from 'assert';
 import extract from './index';
 
-const simple = extract(`
+const input = `
 # title
 
 _24 july 2015_
 
 desc
 
-![](http://yo.io/)
+![alt](http://yo.io/)
 
 content1
 
-content2
-`.trim());
+content2`;
 
-it('simple title', ()=> equal(simple.title.text, 'title'));
-it('simple date',  ()=> equal(simple.date.text,  '24 july 2015'));
-it('simple desc',  ()=> equal(simple.desc.text,  'desc'));
-it('simple image', ()=> equal(simple.image.src,  'http://yo.io/'));
+const article = extract(input, 'DD MMMM YYYY', 'en');
+
+it('title', () =>
+  equal(article.title.text, 'title'));
+
+it('date', ()=>
+  equal(article.date.text,  '24 july 2015'));
+
+it('desc', ()=>
+  equal(article.desc.text,  'desc'));
+
+it('image', () =>
+  equal(article.image.src,  'http://yo.io/'));
+
+it('content', () =>
+  equal(article.content.html,  `<p>desc</p>
+<p><img src="http://yo.io/" alt="alt" /></p>
+<p>content1</p>
+<p>content2</p>
+`));
+
+
+// TODO: tests for invalid input
